22cfb0c590fe81ff998dbec68565163b865853ee
Sergey <georg1303sergey@gmail.com>
2020-08-01 12:24:33 -0700

    Added uikit files

--- gardener/wsgi.py
+++ gardener/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for greenodessa project.
+WSGI config for gardener project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenodessa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gardener.settings')
 
 application = get_wsgi_application()
